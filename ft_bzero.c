3b274beb47112e3b2405c1a018e5c7075b04c053
KijijiKid <mateoandre@MacBook-Pro-von-Mateo.local>
2025-03-10 23:38:44 +0100

    ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,35 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mateoandre <mateoandre@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 22:35:26 by mateoandre        #+#    #+#             */
-/*   Updated: 2025/03/10 23:25:34 by mateoandre       ###   ########.fr       */
+/*   Created: 2025/03/10 23:27:39 by mateoandre        #+#    #+#             */
+/*   Updated: 2025/03/10 23:37:35 by mateoandre       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_memset(char *str, int c, int range)
+void ft_bzero(char *str, int size)
 {
     int i;
     i = 0;
-    while (i < range)
+    while(i < size)
     {
-        str[i] = c;
+        str[i] = '\0';
         i++;
     }
 }
 
 // #include <stdio.h>
-// #include <string.h>
 // int main(void)
 // {
-//     char buffer[10];
-
-//     ft_memset(buffer, 'a', 5);
-//     ft_memset(buffer, 'g', sizeof(char) * 10);
-    // memset(buffer, 'g', sizeof(char) * 10);
+//     char buffer[] = "SeeYou";
     
-//     printf("%s", buffer);
+//     printf("Before: %s\n", buffer);
+//     ft_bzero(buffer, 3);
+//     printf("After: %c", buffer[3]);
 // }
