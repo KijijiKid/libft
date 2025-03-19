0330a75d1b8ae487e2f97d950665a54828ad0ae9
kijijikid <mandre@1-A-2.42heilbronn.de>
2025-03-19 10:40:07 +0100

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,61 +6,12 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:34:07 by mandre            #+#    #+#             */
-/*   Updated: 2025/03/19 10:31:02 by mandre           ###   ########.fr       */
+/*   Updated: 2025/03/19 10:39:43 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s)
-{
-	size_t	i;
-	size_t	s_len;
-	char	*ptr;
-
-	s_len = ft_strlen(s);
-	ptr = (char *)malloc(sizeof(char) * s_len) + 1;
-	if (!(ptr))
-		return (NULL);
-	i = 0;
-	while (s[i] != '\0')
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	size_t			i;
-	unsigned char	*tmp;
-
-
-	tmp = malloc(count * size);
-	if (!tmp)
-		return (NULL);
-	i = 0;
-	while (i < count * size)
-	{
-		tmp[i] = 0;
-		i++;
-	}
-	return (tmp);
-}
-
-size_t	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-/*val_cmp is good*/
 int	val_cmp(char digit, const char *set)
 {
 	int	i;
@@ -82,11 +33,12 @@ char	*s_create(int begin, int end, const char *s1)
 
 	r_string = ft_calloc(end - begin, sizeof(char));
 	i = 0;
-	while (i < end - begin)
+	while (i < end - begin + 1)
 	{
 		r_string[i] = s1[begin + i];
 		i++;
 	}
+	r_string[begin + i] = '\0';
 	return (r_string);
 }
 
@@ -106,10 +58,10 @@ char	*ft_strtrim(const char *s1, const char *set)
 	return (s_create(i, j, s1));
 }
 
-int main(void)
-{
-	char test1[] = "ababababbbHello World!abababbabab";
+// int main(void)
+// {
+// 	char test1[] = "ababababbbHello World!abababbabab";
 
-	printf("Before: %s\n", test1);
-	printf("Before: %s\n", ft_strtrim(test1, "ab"));
-}
+// 	printf("Before: %s\n", test1);
+// 	printf("After: %s\n", ft_strtrim(test1, "ab"));
+// }
