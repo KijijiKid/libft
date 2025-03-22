efe385aca4e31d01b79fcbe64e860bdd2bd98f5f
kijijikid <mandre@3-G-1.42heilbronn.de>
2025-03-22 13:29:22 +0100

    atoi ficed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:03:18 by mandre            #+#    #+#             */
-/*   Updated: 2025/03/22 13:13:15 by mandre           ###   ########.fr       */
+/*   Updated: 2025/03/22 13:28:39 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,31 @@ int	ft_atoi(const char *str)
 	int	multiply;
 
 	i = 0;
-	number = 0;
-	while (str[i] == 32 || 9 <= str[i] || str[i] <= 13)
+	multiply = 1;
+	while (str[i] == 32 || (9 <= str[i] && str[i] <= 13))
 		i++;
-	if (str[i] == '+' && str[i + 1] != '-')
-		i++;
-	if (str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
-		multiply = -1;
+		if (str[i] == '-')
+			multiply = -1;
 		i++;
 	}
+	number = 0;
 	while ('0' <= str[i] && str[i] <= '9')
 	{
 		number *= 10;
 		number += str[i] - '0';
 		i++;
 	}
-	return (number * multiply);
+	number *= multiply;
+	return (number);
 }
 
 // #include <stdio.h>
 // #include <stdlib.h>
 // int	main(void)
 // {
-// 	char arr1[] = "  -23";
+// 	char arr1[] = " \t\v\n\r\f123";
 
 // 	printf("%i", ft_atoi(arr1));
 // }
