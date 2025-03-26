630b60fac778c6889d5d0a884f792ee3569869dd
kijijikid <mandre@3-I-3.42heilbronn.de>
2025-03-26 12:02:17 +0100

    Strmapi not final

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,26 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 15:40:44 by mandre            #+#    #+#             */
-/*   Updated: 2025/03/24 15:56:51 by mandre           ###   ########.fr       */
+/*   Updated: 2025/03/26 12:00:32 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+	char	*array;
+	size_t	i;
+
+	array = calloc(ft_strlen(s) + 1, sizeof(char));
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		array[i] = (*f)(i, s[i]);
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
 }
