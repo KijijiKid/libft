a048435326dac632aa5808fa2e339425d6ba54bb
kijijikid <mandre@3-D-3.42heilbronn.de>
2025-03-30 16:05:53 +0200

    linked list fncts

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 15:46:06 by mandre            #+#    #+#             */
+/*   Updated: 2025/03/30 15:56:46 by mandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	while (*lst != NULL)
+	{
+		temp = *lst;
+		*lst = (*lst)->next;
+		del(temp->content);
+		free(temp);
+	}
+	*lst = NULL;
+}
