e0a05e4777d039610c6c3ae7be8fecd39c1c95e6
kijijikid <mandre@3-D-3.42heilbronn.de>
2025-03-31 15:37:47 +0200

    Some changes

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 15:46:06 by mandre            #+#    #+#             */
-/*   Updated: 2025/03/30 15:56:46 by mandre           ###   ########.fr       */
+/*   Updated: 2025/03/31 15:33:29 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*temp;
 
-	while (*lst != NULL)
+	if (!lst || !del)
+		return ;
+	while (*lst)
 	{
 		temp = *lst;
 		*lst = (*lst)->next;
