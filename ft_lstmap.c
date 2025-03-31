df1b41fda37a2527e39cb2df8a8005d4b6707068
kijijikid <mandre@3-D-3.42heilbronn.de>
2025-03-31 15:15:48 +0200

    Hope is never dead

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mandre <mandre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:25:47 by mandre            #+#    #+#             */
-/*   Updated: 2025/03/31 14:49:16 by mandre           ###   ########.fr       */
+/*   Updated: 2025/03/31 15:14:51 by mandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		tmp = ft_lstnew(f(lst->content));
 		if (!tmp)
 		{
+			if (del)
+				del(tmp->content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
@@ -34,6 +36,48 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	return (new_lst);
 }
 
-// int	main(void)
+// void *double_value(void *content)
 // {
+//     int *new_value = malloc(sizeof(int));
+//     if (!new_value)
+//         return (NULL);
+//     *(new_value) = *(int *)content * 2;
+//     return new_value;
+// }
+// void delete_value(void *content)
+// {
+//     free(content);
+// }
+// void print_list(t_list *lst)
+// {
+//     while (lst)
+//     {
+//         printf("%d -> ", *(int *)lst->content);
+//         lst = lst->next;
+//     }
+//     printf("NULL\n");
+// }
+// void test_ft_lstmap()
+// {
+//     t_list *lst = ft_lstnew(malloc(sizeof(int)));
+//     *(int *)lst->content = 1;
+//     t_list *second = ft_lstnew(malloc(sizeof(int)));
+//     *(int *)second->content = 2;
+//     t_list *third = ft_lstnew(malloc(sizeof(int)));
+//     *(int *)third->content = 3;
+//     lst->next = second;
+//     second->next = third;
+//     third->next = NULL;
+//     printf("Original list:\n");
+//     print_list(lst);
+//     t_list *new_lst = ft_lstmap(lst, double_value, delete_value);
+//     printf("\nNew list after mapping (doubled values):\n");
+//     print_list(new_lst);
+//     ft_lstclear(NULL, delete_value);
+//     ft_lstclear(&new_lst, delete_value);
+// }
+// int main()
+// {
+//     test_ft_lstmap();
+//     return 0;
 // }
